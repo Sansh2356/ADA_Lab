42092c7dc9b817ab018af165cca3145f18c53051
Sansh2356 <sansh2356@gmail.com>
2024-01-30 00:10:22 +0530

    all_TC_lab_1

--- TC_Anaylsis.cpp
+++ TC_Anaylsis.cpp
@@ -14,90 +14,109 @@ using namespace std;
 
 vector<int> generate(int size)
 {
-    vector<int> ele;
-    for (int x = 1; x <= size; x++)
-    {
-        ele.push_back(rand());
-    }
+  vector<int> ele;
+  for (int x = 1; x <= size; x++)
+  {
+    ele.push_back(rand());
+  }
 
-    return ele;
+  return ele;
 }
 void sum(vector<int> arr)
 {
-    long long sum_num = 0;
-    for (auto i : arr)
-    {
-        sum_num += i;
-    }
+  long long sum_num = 0;
+  for (auto i : arr)
+  {
+    sum_num += i;
+  }
 }
 void sel_sort(vector<int> arr)
 {
-    int mini = INT_MAX;
-    int idx = 0;
-    for (int x = 0; x < arr.size(); x++)
-    {
+  int mini = INT_MAX;
+  int idx = 0;
+  for (int x = 0; x < arr.size(); x++)
+  {
 
-        idx = x;
-        mini = arr[x];
-        for (int f = x + 1; f < arr.size(); f++)
-        {
-            if (arr[f] <= mini)
-            {
-                idx = f;
-                mini = arr[f];
-            }
-        }
-        swap(arr[idx], arr[x]);
+    idx = x;
+    mini = arr[x];
+    for (int f = x + 1; f < arr.size(); f++)
+    {
+      if (arr[f] <= mini)
+      {
+        idx = f;
+        mini = arr[f];
+      }
     }
+    swap(arr[idx], arr[x]);
+  }
 }
 void permute(vector<int> arr, vector<int> ele, int idx)
 {
-    if (ele.size() == arr.size())
-    {
-        return;
-    }
-    for (int i = idx; i < arr.size(); i++)
-    {
-        swap(arr[i], arr[idx]);
-        permute(arr, ele, idx + 1);
-        swap(arr[i], arr[idx]);
-    }
+  if (ele.size() == arr.size())
+  {
+    return;
+  }
+  for (int i = idx; i < arr.size(); i++)
+  {
+    swap(arr[i], arr[idx]);
+    permute(arr, ele, idx + 1);
+    swap(arr[i], arr[idx]);
+  }
 }
-void hanoi()
+void towerOfHanoi(int n, char from_rod, char to_rod, 
+                  char aux_rod) 
+{ 
+    if (n == 0) {  
+        return; 
+    } 
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod); 
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod); 
+} 
+void tt_ff(string s, int len)
 {
+  if (s.length() == len)
+  {
+    cout << s << endl;
+    return;
+  }
+  s = s + "T";
+  tt_ff(s, len + 1);
+  s = s + "F";
+  tt_ff(s, len + 1);
 }
-
 void solve()
 {
-    vector<pair<int, double>> time;
-    double avg = 0;
-    for (int x = 6; x <= 10; x += 2)
-    {
+  vector<pair<int, double>> time;
+  double avg = 0;
+  for (int x = 10; x <= 20; x += 2)
+  {
 
-        vector<int> arr = generate(x);
-        int cnt = 10;
-        for (int f = 0; f < 10; f++)
-        {
-            clock_t start = clock();
-            // sel_sort(arr);
-            // sum(arr);
-            permute(arr, {}, 0);
-            clock_t end = clock();
-            avg += (end - start);
-        }
-        time.push_back({x, (double)(avg) / double(cnt)});
-    }
-    for (auto i : time)
+    //vector<int> arr = generate(x);
+    int cnt = 10;
+    for (int f = 0; f < 10; f++)
     {
-        cout << i.first << " " << i.second << endl;
+      clock_t start = clock();
+      // sel_sort(arr);
+      // sum(arr);
+      // permute(arr, {}, 0);
+      //towerOfHanoi(x,'A','B','C');
+      tt_ff("",x);
+      clock_t end = clock();
+      avg += (end - start);
     }
+    time.push_back({x, (double)(avg) / double(cnt)});
+  }
+  for (auto i : time)
+  {
+    cout << i.first << " " << i.second << endl;
+  }
 }
 
 int main()
 {
 
-    // int t;
-    // cin >> t;
+  // int t;
+  // cin >> t;
 
-    solve();
+   solve();
 }
